9f66d4f9237317727c12e0a7d977dbdd805e49a5
hirukawa_ryo <hirukawa_ryo@d83a06de-854e-4732-85b7-fdf7162022b6>
2019-08-26 12:35:55 +0000

    * exewrap-jetty 9.4.20 クラスパスが2重に定義されてしまいクラスロードに失敗するバグを修正しました。
    git-svn-id: http://svn.osdn.net/svnroot/exewrap@67 d83a06de-854e-4732-85b7-fdf7162022b6

--- exewrap-jetty/trunk/src/main/java/exewrap/jetty/Main.java
+++ exewrap-jetty/trunk/src/main/java/exewrap/jetty/Main.java
@@ -1,6 +1,5 @@
 package exewrap.jetty;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -18,19 +17,19 @@ import java.util.UUID;
  * 
  * modified: org/eclipse/jetty/start/Main.java
  * 
- * (1) usageExit method: java -jar $JETTY_HOME/start.jar  ==>  jetty.exe
- * 
- * (2) public static void main(String[] args) の戻り値を boolean に変更して、
+ * (1) public static void main(String[] args) の戻り値を boolean に変更して、
  *     main.start(startArgs) の次行に return startArgs.isRun(); を追加する。
  *     メソッドの末尾には return false; を追加する。
+ * 
+ * (2) usageExit method: java -jar $JETTY_HOME/start.jar  ==>  jetty.exe
  * 
  * modified: src/main/resources/org/eclipse/jetty/start/usage.txt
  * 
  * (2) java -jar start.jar  ==>  jetty.exe
  * 
- * (3) "Startup / Shutdown Command Line" block removed.
+ * (3) Usage 直後の説明文をカットする。
  * 
- * (4) Usage 直後の説明文をカットする。
+ * (4) "Startup / Shutdown Command Line" block removed.
  * 
  * (5) Properties: 説明文の STOP.HOST, STOP.PORT, STOP.KEY, STOP.WAIT を削除する。
  * 
@@ -62,9 +61,11 @@ Service Options:
 
  * (6) META-INF/MANIFEST.MF には以下の内容が必要です。(ビルド時のマニフェストとして指定すること)
  * 無い場合は Bundle-Vendor と Bundle-Version の行をコピーして Bundle の部分を Implementaion にする。
+ * Implementation-Versionをファイル名の一部としてJARファイルをロードするのでとても重要です。
+ * Implementation-Versionが未設定だったり異なっていたりすると適切にJARファイルをロードできず起動に失敗します。
  * 
- * Implementation-Vendor: Eclipse.org - Jetty
- * Implementation-Version: 9.3.7.v20160115
+ * Implementation-Vendor: Eclipse Jetty Project
+ * Implementation-Version: 9.4.20.v20190813
  * 
  */
 
@@ -77,13 +78,30 @@ public class Main {
 	private static String stopKey;
 	private static Object stopMonitor;
 	
+	private static void prepare() {
+		if(System.getProperty("jetty.home") == null) {
+			System.setProperty("jetty.home",
+					System.getProperty("java.application.path"));
+		}
+		if(System.getProperty("jetty.base") == null) {
+			System.setProperty("jetty.base",
+					System.getProperty("user.dir"));
+		}
+		if(System.getProperty("jetty.version") == null) {
+			System.setProperty("jetty.version",
+					Main.class.getPackage().getImplementationVersion());
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
-		args = prepare(args);
+		prepare();
+		
 		org.eclipse.jetty.start.Main.main(args);
 	}
 	
 	public static void start(String[] args) throws IOException, Exception {
-		args = prepare(args);
+		prepare();
+
 		stopPort = String.valueOf(getStopPort());
 		stopKey = UUID.randomUUID().toString();
 		stopMonitor = new Object();
@@ -100,11 +118,12 @@ public class Main {
 	}
 	
 	public static void stop() throws IOException {
-		String[] args = prepare(new String[] { "--stop" });
+		prepare();
+		
 		System.setProperty("STOP.PORT", stopPort);
 		System.setProperty("STOP.KEY", stopKey);
 		
-		org.eclipse.jetty.start.Main.main(args);
+		org.eclipse.jetty.start.Main.main(new String[] { "--stop" });
 		
 		synchronized(stopMonitor) {
 			stopMonitor.notifyAll();
@@ -133,37 +152,7 @@ public class Main {
 		} catch (UnknownHostException e1) {
 			
 		}
+		
 		return port;
-	}
-
-	private static String[] prepare(String[] args) throws IOException {
-		String jettyHome = null;
-		String jettyBase = null;
-		
-		for(int i = 0; i < args.length; i++) {
-			String s = args[i].toLowerCase();
-			if(s.length() > 11 && s.startsWith("jetty.home=")) {
-				jettyHome = new File(args[i].substring(11)).getCanonicalPath();
-				args[i] = "jetty.home=" + jettyHome;
-			}
-			if(s.length() > 11 && s.startsWith("jetty.base=")) {
-				jettyBase = new File(args[i].substring(11)).getCanonicalPath();
-				args[i] = "jetty.base=" + jettyBase;
-			}
-		}
-		
-		if(jettyBase != null) {
-			System.setProperty("user.dir", jettyBase);
-		}
-		
-		if(jettyHome == null) {
-			String[] newArgs = new String[args.length + 1];
-			newArgs[0] = "jetty.home=" + System.getProperty("java.application.path");
-			for(int i = 0; i < args.length; i++) {
-				newArgs[i + 1] = args[i];
-			}
-			return newArgs;
-		}
-		return args;
 	}
 }
